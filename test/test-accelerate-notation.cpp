fbc971c05c04af58411970fd3b77be71ceedb96c
Manya-bansal <manya227@stanford.edu>
2022-08-11 11:42:26 -0700

    test for make reduction notation

--- test/test-accelerate-notation.cpp
+++ test/test-accelerate-notation.cpp
@@ -8,6 +8,20 @@
 
 using namespace taco;
 
+
+static TensorObject as("a", Float64), bs("b", Float64), cs("c", Float64),
+                 ds("d", Float64), es("e", Float64);
+
+static Type vectorType(Float64, {3});
+static TensorObject a("a", vectorType), b("b", vectorType), c("c", vectorType),
+                 d("d", vectorType), e("e", vectorType), f("f", vectorType);
+
+static Type matrixType(Float64, {3,3});
+static TensorObject A("A", matrixType), B("B", matrixType), C("C", matrixType);
+
+static const IndexVar i("i"), j("j"), k("k");
+
+
 TEST(accelerateNotation, AcceleratorExprNode) {
     // AcceleratorExpr((TensorVar()));
     // AcceleratorExpr expr = TensorVar();
@@ -30,4 +44,35 @@ TEST(accelerateNotation, AcceleratorExprNode) {
 
     std::cout << sum(i, x(i)*t(i)) + 5 << endl;
 
+}
+
+TEST(accelerateNotation, makeReductionNotation) {
+  ASSERT_NOTATION_EQ(as = bs*cs,    makeReductionNotation(as = bs*cs));
+  ASSERT_NOTATION_EQ(as = bs*cs*ds, makeReductionNotation(as = bs*cs*ds));
+  ASSERT_NOTATION_EQ(as = bs+ds,    makeReductionNotation(as = bs+ds));
+  ASSERT_NOTATION_EQ(as = bs-ds,    makeReductionNotation(as = bs-ds));
+
+  ASSERT_NOTATION_EQ(as = sum(i, b(i)*c(i)),
+                     makeReductionNotation(as = b(i)*c(i)));
+  ASSERT_NOTATION_EQ(as = sum(i, b(i)*c(i)*d(i)),
+                     makeReductionNotation(as=b(i)*c(i)*d(i)));
+  ASSERT_NOTATION_EQ(as = sum(i, sum(j, b(i)*c(j))),
+                     makeReductionNotation(as=b(i)*c(j)));
+  ASSERT_NOTATION_EQ(as = sum(i, sum(j, sum(k, b(i)*c(j)*d(k)))),
+                     makeReductionNotation(as=b(i)*c(j)*d(k)));
+
+  ASSERT_NOTATION_EQ(as = sum(i, b(i)) + sum(j, c(j)),
+                     makeReductionNotation(as=b(i)+c(j)));
+  ASSERT_NOTATION_EQ(as = sum(i, b(i)) + sum(i, c(i)),
+                     makeReductionNotation(as=b(i)+c(i)));
+  ASSERT_NOTATION_EQ(as = sum(i, b(i)*c(i)) + sum(j, d(j)*e(j)),
+                     makeReductionNotation(as = b(i)*c(i) + d(j)*e(j)));
+  ASSERT_NOTATION_EQ(as = sum(i, b(i)*c(i)) + sum(i, sum(j, d(i)*e(j))),
+                     makeReductionNotation(as = b(i)*c(i) + d(i)*e(j)));
+  ASSERT_NOTATION_EQ(f(i) = b(i)*c(i),
+                     makeReductionNotation(f(i)=b(i)*c(i)));
+  ASSERT_NOTATION_EQ(as = sum(i, sum(j, B(i,j)*C(i,j) )),
+                     makeReductionNotation(as = B(i,j)*C(i,j)));
+  ASSERT_NOTATION_EQ(a(i) = sum(j, B(i,j)*c(j)),
+                     makeReductionNotation(a(i)=B(i,j)*c(j)));
 }
