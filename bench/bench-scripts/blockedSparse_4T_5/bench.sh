ca3aada019cd1b1f3cc24afd5f0c28ab5544c9b9
Manya Bansal <manya227@stanford.edu>
2023-03-08 01:08:54 +0000

    add makefile commands

--- bench/bench-scripts/blockedSparse_4T_5/bench.sh
+++ bench/bench-scripts/blockedSparse_4T_5/bench.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-systems=("5_blas" "5_taco" "5_mkl" "5_gsl")
+systems=("blas" "taco" "mkl" "gsl")
 
 for i in "${systems[@]}"
 do  
-    /home/reviewer/mosaic/build/bin/./taco-bench --benchmark_filter=bench_blockedSparse4T_$i  --benchmark_format=json --benchmark_out=/home/reviewer/mosaic/bench/bench-scripts/blockedSparse_4T_5/result/$i
+    /home/reviewer/mosaic/build/bin/./taco-bench --benchmark_filter=bench_blockedSparse4T_5_$i  --benchmark_format=json --benchmark_out=/home/reviewer/mosaic/bench/bench-scripts/blockedSparse_4T_5/result/$i
 done
